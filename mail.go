02add19932b2f7d3c7549121f26e05b9f2309d70
souvikhaldar <souvikhaldar32@gmail.com>
2019-01-06 00:53:47 +0530

    to,sub,body in sendmail

--- mail.go
+++ mail.go
@@ -8,14 +8,11 @@ import (
 
 type Config struct {
 	From     string
-	To       []string
-	Subject  string
-	Body     string
 	Password string
 }
 
-func New(from string, to []string, password string) (error, *Config) {
-	if from == "" || to == nil {
+func New(from string, password string) (error, *Config) {
+	if from == "" {
 		return errors.New("To and from field can't be empty"), &Config{}
 	}
 	return nil, &Config{
@@ -36,11 +33,14 @@ func (c *Config) CreateMsg(To []string, Subject, Body string) []string {
 	return msgString
 }
 
-func (c *Config) SendMail(to []string, subject, body string) error {
-	msgSlice := c.CreateMsg(to, subject, body)
+func (c *Config) SendMail(To []string, subject, body string) error {
 	auth := smtp.PlainAuth("", c.From, c.Password, "smtp.gmail.com")
-	for _, msg := range msgSlice {
-		err := smtp.SendMail("smtp.gmail.com:587", auth, "joeymartin367@gmail.com", c.To, []byte(msg))
+	for _, to := range To {
+		msg := fmt.Sprintf("To: %s \r\n"+
+			"Subject: %s\r\n"+
+			"\r\n"+
+			"%s\r\n", to, subject, body)
+		err := smtp.SendMail("smtp.gmail.com:587", auth, c.From, []string{to}, []byte(msg))
 		if err != nil {
 			return err
 		}
