ea4e3208245a688ad9c4eaa6413c1b6fa8456e1f
souvikhaldar <souvikhaldar32@gmail.com>
2019-01-06 00:43:38 +0530

    to added to sendmail

--- mail.go
+++ mail.go
@@ -20,14 +20,13 @@ func New(from string, to []string, password string) (error, *Config) {
 	}
 	return nil, &Config{
 		From:     from,
-		To:       to,
 		Password: password,
 	}
 }
 
-func (c *Config) CreateMsg(Subject, Body string) []string {
-	msgString := make([]string, len(c.To))
-	for i, to := range c.To {
+func (c *Config) CreateMsg(To []string, Subject, Body string) []string {
+	msgString := make([]string, len(To))
+	for i, to := range To {
 		msg := fmt.Sprintf("To: %s \r\n"+
 			"Subject: %s\r\n"+
 			"\r\n"+
@@ -37,8 +36,8 @@ func (c *Config) CreateMsg(Subject, Body string) []string {
 	return msgString
 }
 
-func (c *Config) SendMail(subject, body string) error {
-	msgSlice := c.CreateMsg(subject, body)
+func (c *Config) SendMail(to []string, subject, body string) error {
+	msgSlice := c.CreateMsg(to, subject, body)
 	auth := smtp.PlainAuth("", c.From, c.Password, "smtp.gmail.com")
 	for _, msg := range msgSlice {
 		err := smtp.SendMail("smtp.gmail.com:587", auth, "joeymartin367@gmail.com", c.To, []byte(msg))
