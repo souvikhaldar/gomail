9292a8564564434a2cbe9f583b1fb80ea3c9f07b
souvikhaldar <souvikhaldar32@gmail.com>
2019-01-05 23:30:27 +0530

    subject, body passed in sendmail

--- mail.go
+++ mail.go
@@ -14,33 +14,31 @@ type Config struct {
 	Password string
 }
 
-func New(from string, to []string, subject string, body string, password string) (error, *Config) {
+func New(from string, to []string, password string) (error, *Config) {
 	if from == "" || to == nil {
 		return errors.New("To and from field can't be empty"), &Config{}
 	}
 	return nil, &Config{
 		From:     from,
 		To:       to,
-		Subject:  subject,
-		Body:     body,
 		Password: password,
 	}
 }
 
-func (c *Config) CreateMsg() []string {
+func (c *Config) CreateMsg(Subject, Body string) []string {
 	msgString := make([]string, len(c.To))
 	for i, to := range c.To {
 		msg := fmt.Sprintf("To: %s \r\n"+
 			"Subject: %s\r\n"+
 			"\r\n"+
-			"%s\r\n", to, c.Subject, c.Body)
+			"%s\r\n", to, Subject, Body)
 		msgString[i] = msg
 	}
 	return msgString
 }
 
-func (c *Config) SendMail() error {
-	msgSlice := c.CreateMsg()
+func (c *Config) SendMail(subject, body string) error {
+	msgSlice := c.CreateMsg(subject, body)
 	auth := smtp.PlainAuth("", c.From, c.Password, "smtp.gmail.com")
 	for _, msg := range msgSlice {
 		err := smtp.SendMail("smtp.gmail.com:587", auth, "joeymartin367@gmail.com", c.To, []byte(msg))
